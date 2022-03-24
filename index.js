e3726d7246a2575fbb59a577abfe7aad095ed9f4
andrey <smooker14@gmail.com>
2022-03-24 16:04:15 +0200

    commit

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bodyParser = require('body-parser')
 const port = 3000;
 
 app.use(cors());
+const jsonParser = bodyParser.json()
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
-app.post('/test', (req, res) => {
-    console.log(req);
+app.post('/test', urlencodedParser, (req, res) => {
+    console.log(req.body);
     res.json({msg: 'This is CORS-enabled for an allowed domain. POST'})
 })
 
